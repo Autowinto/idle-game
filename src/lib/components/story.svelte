3cf70c51a714a034c80953c232d8dab43bb43613
Aloxion <ansan21@student.sdu.dk>
2025-03-24 14:14:17 +0100

    Basic UI and UX

---
+++ src/lib/components/story.svelte
@@ -0,0 +1,3 @@
+<div class="content p-6">
+  <p class="text-lg font-bold">The sun rises on the first day of the eternal cycle. The world is new and full of promise...</p>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import "../app.css";
+  import Stats from "$lib/components/stats.svelte";
+  import Story from "$lib/components/story.svelte";
+</script>
+
+<div class="container">
+  <h1 class="text-4xl font-bold mb-8">The Eternal Cycle</h1>
+  <div class="main gap-4">
+    <Stats />
+    <Story />
+  </div>
+</div>
