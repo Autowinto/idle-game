9a0d37b051a34f9dcb18af7bf5264974a4b95153
Aloxion <58940157+Aloxion@users.noreply.github.com>
2025-03-24 18:24:47 +0100

    Update inventory.svelte

--- src/lib/components/inventory.svelte
+++ src/lib/components/inventory.svelte
@@ -1,8 +1,42 @@
-<div class="story p-6">
-  <h1 class="text-4xl font-bold mb-4">Inventory</h1>
+<div>
+  {#each items as item}
+    <div class=' bg-black rounded mb-3 p-3 flex gap-3'>
+      <image src={item.iconPath} class='bg-white max-w-8 max-h-8' />
+      <div>
+        <div class='text-xl font-bold'>{item.name}</div>
+        <div>{item.description}</div>
+        <div>Quantity: {item.quantity}</div>
 
-  <p class="text-lg font-bold">
-    The sun rises on the first day of the eternal cycle. The world is new and
-    full of promise...
-  </p>
+      </div>
+    </div>
+  {/each}
 </div>
+
+<script lang='ts'>
+  import type { Item, Weapon } from '$lib/types'
+  import defImg from '$lib/assets/notfound.png'
+
+  const items: Item[] = []
+
+  const blaster: Weapon = {
+    id: 'weapon:blaster',
+    name: 'Blaster',
+    description: 'A standard issue blaster',
+    quantity: 1,
+    stackable: false,
+    iconPath: defImg,
+    damage: 2,
+  }
+
+  const potion: Item = {
+    id: 'consumable:potion',
+    name: 'Potion',
+    description: 'A healing potion',
+    quantity: 3,
+    iconPath: defImg,
+    stackable: true,
+  }
+
+  items.push(blaster)
+  items.push(potion)
+</script>
